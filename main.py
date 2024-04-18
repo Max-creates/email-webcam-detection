29564e27216d8db061077200146b45e86e9a0f33
Max <maksym.solomyanov@gmail.com>
2024-04-18 19:24:30 +0200

    Add grayframe and grayframegaublur.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
-    check, print_frame = video.read()
-    cv2.imshow("My video", print_frame)
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (11, 11), 0)
+    cv2.imshow("My video", gray_frame_gau)
+    
+    if first_frame is None:
+        first_frame = gray_frame_gau
     
     key = cv2.waitKey(1)
     
