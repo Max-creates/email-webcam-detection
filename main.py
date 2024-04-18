1c521a348534ff48e22b37e313807010622d9b01
Max <maksym.solomyanov@gmail.com>
2024-04-18 19:33:08 +0200

    Add thresh frame.

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ while True:
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (11, 11), 0)
-    cv2.imshow("My video", gray_frame_gau)
     
     if first_frame is None:
         first_frame = gray_frame_gau
         continue
         
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-    
+    thresh_frame = cv2.threshold(delta_frame, 65, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow("My video", thresh_frame)
     
     key = cv2.waitKey(1)
     
